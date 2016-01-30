f7dbf07ae8c9b4db7fe0d3318da00bd0dd81ec28
Spikerman <spikechenhao@163.com>
2016-01-30 19:59:26 +0800

    initial

---
+++ ActiveDetection/platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,68 @@
+angular.module('starter.controllers', ["ngCordova"])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function(chat) {
+        Chats.remove(chat);
+    }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope,$cordovaDeviceMotion) {
+    $scope.settings = {
+        enableFriends: true
+    };
+
+    $scope.data = {
+        x: 0,
+        y: 0,
+        z: 0
+
+    }
+
+   
+
+    $scope.getAcc = function () {
+      
+  
+
+      $cordovaDeviceMotion.getCurrentAcceleration().then(success, fail);
+      function success(acc){
+
+         $scope.data= acc;
+         
+
+      }
+      function fail(reason){
+          alert(JSON.stringify(reason));
+      }
+      
+
+    }
+
+
+
+        var options = { frequency: 300 };
+        var watchId;
+
+        watchId = $cordovaDeviceMotion.watchAcceleration(options).then(null, fail, notify);
+
+        function notify(acc) {
+
+            $scope.data = acc;
+
+
+        }
+
+        function fail(reason) {
+            alert(JSON.stringify(reason));
+        }
+     
+    
+
+});
